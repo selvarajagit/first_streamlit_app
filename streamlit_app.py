9316aa0e64cfd40ff5e8c1b6e2b30418db0e0303
selvarajagit <selvarajagit@users.noreply.github.com>
2023-08-04 13:49:56 -0400

    Added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('this is first time I am learning all this')
-streamlit.header('My main goals')
-streamlit.text('get proficient into snowflake')
-streamlit.text('get proficient into databricks')
-streamlit.text('get certified in az900')
-streamlit.text('get certified in aws')
+streamlit.header('My main goals  🥣')
+streamlit.text('get proficient into snowflake 🥗')
+streamlit.text('get proficient into databricks 🐔')
+streamlit.text('get certified in az900 🥑')
+streamlit.text('get certified in aws 🍞')
 
