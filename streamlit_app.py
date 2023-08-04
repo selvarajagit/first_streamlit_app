b8e5a8fd1c4077146735aff64337d00e9b47aa70
selvarajagit <selvarajagit@users.noreply.github.com>
2023-08-04 13:57:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('this is first time I am learning all this')
 streamlit.header('My main goals  🥣')
 streamlit.text('get proficient into snowflake 🥗')
@@ -7,3 +8,8 @@ streamlit.text('get certified in az900 🥑')
 streamlit.text('get certified in aws 🍞')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe = my_fruit_list
