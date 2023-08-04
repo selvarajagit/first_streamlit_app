d5b7ff5e6bed00af1dea9197bb8f0960201a998d
selvarajagit <selvarajagit@users.noreply.github.com>
2023-08-04 13:58:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,8 @@ streamlit.text('get proficient into snowflake 🥗')
 streamlit.text('get proficient into databricks 🐔')
 streamlit.text('get certified in az900 🥑')
 streamlit.text('get certified in aws 🍞')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.dataframe = my_fruit_list
