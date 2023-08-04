42136bfd46207f33a6202224f60c726c2d946f2a
selvarajagit <selvarajagit@users.noreply.github.com>
2023-08-04 13:46:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('this is first time I am learning all this')
 streamlit.header('My main goals')
-steamlist.header('get proficient into snowflake')
-steamlist.header('get proficient into databricks')
-steamlist.header('get certified in az900')
-steamlist.header('get certified in aws')
+streamlit.header('get proficient into snowflake')
+streamlit.header('get proficient into databricks')
+streamlit.header('get certified in az900')
+streamlit.header('get certified in aws')
 
