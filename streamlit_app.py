5ec84f160beb08ff2b8974333b4c3d062bd05cce
selvarajagit <selvarajagit@users.noreply.github.com>
2023-08-04 14:50:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ streamlit.text('get certified in aws 🍞')
 streamlit.text('get certified in snowflake 🍞')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt", header=1)
+my_fruit_list = my_fruit_list.set_index("Fruit")
 streamlit.multiselect("pick some fruit list", list(my_fruit_list.index))
 streamlit.dataframe[my_fruit_list]
 
