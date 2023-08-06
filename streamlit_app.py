4032d13df2722f08393dc1983f1952d6d4dcdb25
selvarajagit <selvarajagit@users.noreply.github.com>
2023-08-06 13:25:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,14 @@ my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
+
+#streamlit.text(fruityvice_response.json())
+
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+
 # write your own comment - what does this do?
 streamlit.dataframe[fruityvice_normalized]
 
