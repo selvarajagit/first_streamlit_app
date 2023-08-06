33c759b4b6bbfeed3d2926c72d0dbb6f3216f97a
selvarajagit <selvarajagit@users.noreply.github.com>
2023-08-06 14:00:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame(
-   np.random.randn(10, 5),
-   columns=('col %d' % i for i in range(5)))
+   np.random.randn(50, 20),
+   columns=('col %d' % i for i in range(20)))
 
-st.table(df)
+st.dataframe(df)  # Same as st.write(df)
