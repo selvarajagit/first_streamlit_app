701211f4b19b250900f022613c0a036042e71724
selvarajagit <selvarajagit@users.noreply.github.com>
2023-08-08 14:59:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.text("Fruit load list contains")
-streamlit.text(my_data_row)
+streamlit.text(my_data_rows)
 
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
