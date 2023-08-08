0008e6883ddf213d6c2f96574c8afe7071aeb4a3
selvarajagit <selvarajagit@users.noreply.github.com>
2023-08-08 14:55:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,6 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 
-
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
